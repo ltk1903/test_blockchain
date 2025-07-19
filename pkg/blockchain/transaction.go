d5dfa39983828dfe74b7b18e7a72f21e187045ba
ltk1903 <khangltk19032002@gmail.com>
2025-07-19 11:02:42 +0700

    define block transaction

---
+++ pkg/blockchain/transaction.go
@@ -0,0 +1,55 @@
+package blockchain
+
+import (
+	"time"
+)
+
+type Transaction struct {
+	From string
+	To string
+	Amount float
+	Timestamp int64
+	Signature string
+}
+
+// type Block struct {
+// 	Index int
+// 	Timestamp time.Time
+// 	Transaction []Transaction
+// 	PrevHash string
+// 	Hash string
+// 	MerkleRoot string
+// 	Validator string
+// 	Signature string
+// 
+
+func NewTransaction(from, to string, amount float) *Transaction {
+	return &Transaction{
+		From: from,
+		To: to,
+		Amount: amount,
+		Timestamp: time.Now().Unix(),
+	}
+}
+
+func(tx *Transaction) Hash() []byte {
+	copyTx := *tx
+	copyTx.Signature = nil
+	bytes, _:= json.Marshal(copyTx)
+	hash := sha256.Sum256(bytes)
+	return hash[:]
+}
+
+func (tx *Transaction) Sign(priv *ecdsa.PrivateKey) error {
+	hash := tx.Hash()
+	sig, err := wallet.Sign(hash, priv)
+	if err != nil {
+		return err
+	}
+	tx.Signature = sig
+	return nil
+}
+
+func (tx *Transaction) Verify(pub *ecdsa.PublicKey) bool {
+	return wallet.Verify(tx.Hash(), tx.Signature, pub)
+}
